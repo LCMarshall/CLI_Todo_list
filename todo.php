d5be94272f0d56489da970c145c59b53e23ec910
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-20 10:14:50 -0500

    added push and unshift

--- todo.php
+++ todo.php
@@ -2,18 +2,56 @@
 
 // Create array to hold list of todo items
 $items = array('');
-unset($items[0]);
+// unset($items[0]);
 
 /// List array items formatted for CLI
 function list_items($list){
     $result = '';
 
     foreach ($list as $key => $value) {
-        $result .= "[{$key}] $value" . PHP_EOL;
+        $result .= "[" . ($key + 1) . "]" . $value . PHP_EOL;
     }
     
     return $result;
 }
+
+function sort_menu ($items) {
+    
+
+    echo '(A)-Z, (Z)-A, (O)rder entered (R)everse order entered:';
+        $input = get_input(true);
+    
+    if ($input == 'A') {
+        asort($items);
+    } elseif ($input == 'Z') {
+        arsort($items);
+    } elseif ($input == 'O') {
+        ksort($items);
+    } elseif ($input == 'R') {
+        krsort($items);
+
+    }
+    return $items;
+
+}
+
+// When a new item is added to a TODO list, 
+// ask the user if they want to add it to the beginning or end of the list. 
+// Default to end if no input is given.
+
+// Allow a user to enter F at the main menu to remove the first item 
+// on the list. This feature will not be added to the menu, 
+// and will be a special feature that is only available to "power users". 
+// Also add a L option that grabs and removes the last item in the list.
+// Add a (S)ort option to your menu. When it is chosen, 
+// it should call a function called sort_menu().
+
+// When sort menu is opened, show the following options 
+// "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered".
+
+// When a sort type is selected, order the TODO list accordingly 
+// and display the results.
+
     // Return string of list items separated by newlines.
     // Should be listed [KEY] Value like this:
     // [1] TODO item 1
@@ -39,11 +77,6 @@ function get_input($upper = FALSE) {
     // }
     // Return filtered STDIN input
 
-
-
-
-
-
 // The loop!
 do {
     // // Iterate through list items
@@ -51,27 +84,44 @@ do {
     //     // Display each item and a newline
     //     echo "[{$key}] {$item}\n";
     // }
-    echo list_items($items);
+    
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+
+    echo '(N)ew item, (R)emove item, (S)ort, (Q)uit : ';
+
+    $input = get_input(TRUE); 
+    echo list_items($items);   
 
     // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = get_input(TRUE);
+    // Use trim() to remove whitespace and newlines   
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = get_input();
+        $item = get_input();
+        echo "Would you like to add this to item to the (B)eginning or (E)nd of your ToDo list?";
+        $input = get_input(TRUE);
+        if ($input == 'B'){
+            array_unshift($items, $item);
+            } elseif ($input == 'E') {
+            array_push($items, $item);
+            } else {
+            array_push($items, $item);
+        }       
+
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = get_input();
+        $item = get_input();
         // Remove from array
-        unset($items[$key]);
+        unset($items[$key - 1]);
+        // $items = array_values($items);
+    } elseif ($input == 'S') {
+        $items = sort_menu($items);
     }
 // Exit when input is (Q)uit
 } while ($input != 'Q');
